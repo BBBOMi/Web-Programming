6e0fc6ba89cd22038b9b03ecdfbf04dc5f3bf5da
bomi <a51022866@gmail.com>
2019-05-15 15:42:00 +0900

    Create Data Transfer Object

---
+++ web1_Spring/mybatis1/src/main/java/net/skhu/dto/Student.java
@@ -0,0 +1,61 @@
+package net.skhu.dto;
+
+/**
+ * Created by bomi on 2019-05-15.
+ */
+public class Student {
+    private int id;
+    private String studentNumber;
+    private String name;
+    private int departmentId;
+    private int year;
+    private String departmentName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+}
