45d11d6231c3ae3f134728f5200971e37b42d522
Bomi <a51022866@gmail.com>
2019-10-13 22:40:49 +0900

    project init commit

---
+++ web2_Spring/jpa11/src/main/java/net/skhu/jpa11/Jpa11Application.java
@@ -0,0 +1,13 @@
+package net.skhu.jpa11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jpa11Application.class, args);
+    }
+
+}
